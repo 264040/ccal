c4cbf1052e30187cbe36dfadc2c74e1858bbd2d8
Acer <2640402754@qq.com>
2025-09-03 17:22:18 +0800

    首次提交

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
-<script setup lang="ts">
-import { ref, watch } from "vue";
+<script setup lang="ts"> 
 import { useRouter } from "vue-router";
 import AppBottomNavigation from "@/components/Navigation.vue";
 
